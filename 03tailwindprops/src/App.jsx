903de1b608ed02ad79b3063ea5a8df4cbfcbd767
Moteeullah Azmi <you@example.com>
2024-02-22 15:28:28 +0530

    03Tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <Card userName='Hitesh Chaudhary' images="https://imgs.search.brave.com/U6c8mJE67ed8-3h4fVi0_lLRpRHCYLO-mue0vn8J5XU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly95dDMu/Z2dwaHQuY29tL2Ev/QUFUWEFKeWdKclQy/Y3M2aktGV1UxdU5D/Ri1sODVtdlg1Ni1l/bU9XY1R3PXM4MDAt/Yy1rLWMweGZmZmZm/ZmZmLW5vLXJqLW1v.jpeg"/>
+    <Card userName='Moteeullah Azmi' profile="Open" images="https://avatars.githubusercontent.com/u/61102569?v=4"/>
     </>
   )
-}
 
+  }
 export default App
