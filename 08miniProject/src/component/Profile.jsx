e211b25874ec38adf0198a62c211c403de274ae6
Moteeullah Azmi <you@example.com>
2024-03-04 15:56:35 +0530

    context

--- 08miniProject/src/component/Profile.jsx
+++ 08miniProject/src/component/Profile.jsx
@@ -3,7 +3,7 @@ import UserContext from '../context/UserContext'
 
 
 function Profile() {
-  const {user} = useContext(UserContext)
+  const {user} = userContext(UserContext)
 
   if(!user) return <div>please login</div>
   return <div>Welcome {user.username}</div>
